579a5e5a56f4ad2fbbd16271e8709c3bfba17217
Hiresh <hbremanand4@gmail.com>
2022-09-04 21:57:55 +0100

    initial commit

--- server/routes/mail.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const {sendMail} = require("../controllers/mail")
-
-router.post("/sendmail" , (req,res) => {sendMail});
-
-module.exports = router;
